20d1a6b11a9ac3797383155b82dae95947cce973
Kartikesh <44187008+kartikesh5232@users.noreply.github.com>
2023-07-06 14:15:59 +0530

    Initial commit

---
+++ src/main/java/com/example/eudiscoveryservice/EuDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.eudiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EuDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuDiscoveryServiceApplication.class, args);
+	}
+
+}
