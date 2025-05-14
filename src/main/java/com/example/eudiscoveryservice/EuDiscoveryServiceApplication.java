4fd1a50a80ba06189b65d9d4cff177bb8a047c76
Kartikesh <44187008+kartikesh5232@users.noreply.github.com>
2025-05-14 17:32:25 +0530

    Update EuDiscoveryServiceApplication.java

--- src/main/java/com/example/eudiscoveryservice/EuDiscoveryServiceApplication.java
+++ src/main/java/com/example/eudiscoveryservice/EuDiscoveryServiceApplication.java
@@ -3,12 +3,11 @@ package com.example.eudiscoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EuDiscoveryServiceApplication {
-
-
 
 	
 	public static void main(String[] args) {
@@ -16,5 +15,7 @@ public class EuDiscoveryServiceApplication {
 
 		
 	}
+
+	
 
 }
